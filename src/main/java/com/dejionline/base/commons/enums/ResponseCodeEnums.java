a7437ea523ddd52f2b183c0c24a30e1098657bfc
shengyang <123456>
2016-04-06 18:48:55 +0800

    代码优化

--- src/main/java/com/dejionline/base/commons/enums/ResponseCodeEnums.java
+++ src/main/java/com/dejionline/base/commons/enums/ResponseCodeEnums.java
@@ -7,7 +7,6 @@ package com.dejionline.base.commons.enums;
  */
 public enum ResponseCodeEnums {
 
-    //调用成功
     SUCCESS(-1, "接口调用成功"),
 
     RUNTIME_ERROR(-2, "程序运行时异常"),
@@ -18,48 +17,20 @@ public enum ResponseCodeEnums {
 
     PARAMS_EMPTY(-8, "参数为空"),
 
-    //插入数据库失败
     DATABASE_ERROR(-22, "数据库操作失败"),
 
-    //操作购物车缓存失败
     CACHE_ERROR(-23, "更新缓存失败"),
 
-    //调用其它服务异常
     OTHER_SERVICE_ERROR(-202, "调用其它服务接口异常"),
 
-    //调用其它服务超时
     OTHER_SERVICE_TIMEOUT(-203, "调用其它服务接口超时"),
 
     GET_HTTPCLIENT_POOL_TIMEOUT(-204, "httpclient从连接池获取连接超时"),
 
     ORDER_QUERY_NO_DATA(-205, "订单查询无数据"),
 
-    //没有权限
-    NO_PERMISSION(99, "没有权限"),
-
-    //创建订单时，有商品无法购买
-    EXISTS_CAN_NOT_BUY_ITEMS(200, "存在无法购买的商品，无法创建订单"),
-
-    //创建订单时，订单信息有误
-    ORDER_INFO_NOT_MATCH(201, "订单信息有误，无法创建订单"),
-
-    //购买商品超过限购数量
-    OVER_ITEM_LIMIT_QUANTITY(202, "超过商品限购数量"),
-
-    //锁未释放
-    LOCK_HAS_BEEN_ACQUIRED(203, "分布式锁未释放"),
-
-    //该订单状态无法进行此操作
-    ORDER_STATUS_ERROR(204, "该订单状态无法进行此操作"),
-
-    REQUEST_PAY_ERROR(205, "创建订单成功,请求支付失败"),
-
-    LOCK_TIME_OUT_AND_AUTO_RELEASE(206, "分布式锁内业务执行超时，自动释放"),
-
-    //支付流水不存在
     PAY_INFO_NOT_FOUND_BY_PAY_SERIAL(-300, "根据支付流水无法查询到订单"),
 
-    //支付信息与支付方返回信息不一致
     PAY_INFO_NOT_MATCH(-301, "支付信息与支付方返回信息不一致"),
 
     RSA_SIGN_ERROR(-302, "RSA加密错误"),
@@ -72,7 +43,23 @@ public enum ResponseCodeEnums {
 
     VERIFY_SIGN_ERROR(-306, "验签错误"),
 
-    GET_UNIONPAY_TN_ERROR(-307, "获取银联TN号失败");
+    GET_UNIONPAY_TN_ERROR(-307, "获取银联TN号失败"),
+
+    NO_PERMISSION(99, "没有权限"),
+
+    EXISTS_CAN_NOT_BUY_ITEMS(200, "存在无法购买的商品，无法创建订单"),
+
+    ORDER_INFO_NOT_MATCH(201, "订单信息有误，无法创建订单"),
+
+    OVER_ITEM_LIMIT_QUANTITY(202, "超过商品限购数量"),
+
+    LOCK_HAS_BEEN_ACQUIRED(203, "分布式锁未释放"),
+
+    ORDER_STATUS_ERROR(204, "该订单状态无法进行此操作"),
+
+    REQUEST_PAY_ERROR(205, "创建订单成功,请求支付失败"),
+
+    LOCK_TIME_OUT_AND_AUTO_RELEASE(206, "分布式锁内业务执行超时，自动释放");
 
     // 接口返回编码
     private int code;
